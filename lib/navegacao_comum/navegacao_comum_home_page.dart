392870f4d3367e9655df49eb7da507470fc8317d
victor <victor.alexandre@atlantasistemas.com>
2023-07-02 12:26:14 -0300

    Get rotas usando TO

---
+++ lib/navegacao_comum/navegacao_comum_home_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_rotas/navegacao_comum/to/to_home_page.dart';
+
+class NavegacaoComumHomePage extends StatelessWidget {
+  const NavegacaoComumHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        //backgroundColor: Colors.blue,
+        title: const Align(
+          alignment: Alignment.center,
+          child: Text(
+            'Home Navegação Comum',
+          ),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Get.to(const ToHomePage());
+              },
+              style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.purple),
+                  overlayColor:
+                      MaterialStateProperty.all<Color>(Colors.purple.shade600)),
+              child: const Text(
+                'TO (Push)',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/navegacao_comum/to/to_navegacao_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ToNavegacaoPage extends StatelessWidget {
+  const ToNavegacaoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Page de Navegação'),
+      ),
+      body: Container(),
+    );
+  }
+}
